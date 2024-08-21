7946f7f5dd5edbd3c301c44af8e9f4794eb6ebaa
denysmerts <denysmerts@gmail.com>
2024-08-21 14:42:57 +0100

    update translation & add stories for a MainScreen

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,14 +3,17 @@
 import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
 import enTranslation from "./locales/en/translation.json";
 import esTranslation from "./locales/es/translation.json";
 
 i18n
+  .use(initReactI18next)
   .use(Backend) // lazy loads translations from /public/locales
   .use(LanguageDetector) // detect user language
   .init({
+    lng: "en",
     fallbackLng: "en",
     debug: true,
     interpolation: {
---
+++ src/screens/MainScreen/MainScreen.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { MainScreen } from "./MainScreen";
+
+// Define the meta information for Storybook
+const meta: Meta<typeof MainScreen> = {
+  title: "MainScreen",
+  component: MainScreen,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MainScreen>;
+
+export const Default: Story = {
+  render: () => <MainScreen />,
+};
